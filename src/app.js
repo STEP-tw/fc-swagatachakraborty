c42b2ee9c024865f8b4e912f92a481ceae3ce700
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-01-16 14:56:18 +0530

    use downloaded gif instead of using link.
    change styling.
    use external css instead of inline css.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const fs = require("fs");
 
 const app = (req, res) => {
+  const reader = function(error, data) {
+    res.write(data);
+    res.end();
+  };
+
   if (req.url == "/") {
-    fs.readFile("./src/flowerCatalog.html", "utf8", (err, data) => {
-      res.write(data);
-      res.end();
-    });
+    fs.readFile("./src/flowerCatalog.html", "utf8", reader);
   }
 
-  if (req.url == "/src/images/flowers.jpg") {
-    fs.readFile("./src/images/flowers.jpg", (err, data) => {
-      res.write(data);
-      res.end();
-    });
+  if (req.url == "/images/flowers.jpg") {
+    fs.readFile("./src/images/flowers.jpg", reader);
+  }
+
+  if (req.url == "/images/watering_jar.gif") {
+    fs.readFile("./src/images/watering_jar.gif", reader);
+  }
+
+  if (req.url == "/style.css") {
+    fs.readFile("./src/style.css", "utf8", reader);
   }
 };
 
