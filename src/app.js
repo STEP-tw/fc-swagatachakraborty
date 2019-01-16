146bada1b654319389fea88203fe3b52843194b2
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-01-16 13:08:25 +0530

    server will give web page with one static image and gif with some link.
    the gif is not yet hideing while clinking on it.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
-const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
-};
+const fs = require("fs");
 
-// Export a function that can act as a handler
+const app = (req, res) => {
+  if (req.url == "/") {
+    fs.readFile("./src/flowerCatalog.html", "utf8", (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  }
+
+  if (req.url == "/src/images/flowers.jpg") {
+    fs.readFile("./src/images/flowers.jpg", (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  }
+};
 
 module.exports = app;
