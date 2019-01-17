4c6473bb14ef34263459214af99d2dba93c2413e
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-01-17 15:22:55 +0530

    use a frame work for handle valid requests.

--- src/app.js
+++ src/app.js
@@ -1,26 +1,45 @@
 const fs = require("fs");
+const Handler = require("./requestHandler");
+const app = new Handler();
 
 const getFIlePath = url => "./public" + url;
 
-const send = function (res, status, content) {
-	res.statusCode = status;
-	res.write(content);
-	res.end();
+const logger = function(req, res) {
+  console.log("URL:", req.url);
+  console.log("Headers:", req.headers);
+  console.log("-------------------------------------------------------------");
+};
+
+const send = function(res, status, content) {
+  res.statusCode = status;
+  res.write(content);
+  res.end();
 };
 
 const reader = function(res, error, data) {
-	let status = 200;
-	if(error){
-		status = 404;
-		data = '404: Not Found';
-	}
-	send(res, status, data);
+  let status = 200;
+  if (error) sendNotFound(res);
+  send(res, status, data);
 };
 
-const app = (req, res) => {
-	let url = getFIlePath(req.url);
-	if (req.url == "/") url = "./public/flowerCatalog.html";
-	fs.readFile(url, reader.bind(null, res));
+const sendNotFound = function(res) {
+  send(res, 404, "404: Not Found");
 };
 
-module.exports = app;
+const renderHome = (req, res) => {
+  fs.readFile("./public/flowerCatalog.html", reader.bind(null, res));
+};
+
+const serveFile = function(req, res) {
+  let url = getFIlePath(req.url);
+  fs.readFile(url, reader.bind(null, res));
+};
+
+app.view(logger);
+app.get("/", renderHome);
+app.get("/style.css", serveFile);
+app.get("/jarController.js", serveFile);
+app.get("/images/flowers.jpg", serveFile);
+app.get("/images/watering_jar.gif", serveFile);
+
+module.exports = app.handleRequests.bind(app);
---
+++ src/requestHandler.js
@@ -0,0 +1,34 @@
+const areMatching = function(req, route) {
+  if (route.method && req.method != route.method) return false;
+  if (route.url instanceof RegExp && route.url.test(req.url)) return true;
+  if (route.url && route.url != req.url) return false;
+  return true;
+};
+
+class Handler {
+  constructor(url = []) {
+    this.routes = url;
+  }
+
+  handleRequests(req, res) {
+    let validRequests = this.routes.filter(route => areMatching(req, route));
+    function next() {
+      let currentReq = validRequests.shift();
+      if (!currentReq) return;
+      currentReq.handler(req, res);
+      next();
+    }
+
+    next();
+  }
+
+  view(handler) {
+    this.routes.push({ handler });
+  }
+
+  get(url, handler) {
+    this.routes.push({ url, handler });
+  }
+}
+
+module.exports = Handler;
