14df52da68974d4f67224685b3780ce39f05cced
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-01-18 15:34:13 +0530

    Add two more pages in the links.
    public/abeliophyllum.html
    public/ageratum.html

--- src/app.js
+++ src/app.js
@@ -13,11 +13,14 @@ app.view(readBody);
 app.view(logger);
 app.get("/", renderHome);
 app.get("/style.css", serveFile);
-app.post("/style.css", serveFile);
 app.get("/jarController.js", serveFile);
 app.get("/images/flowers.jpg", serveFile);
 app.get("/guest_book.html", renderGuestBook);
 app.get("/images/watering_jar.gif", serveFile);
 app.post("/guest_book.html", storeComment);
+app.get("/ageratum.html", serveFile);
+app.get("/images/ageratum.jpg", serveFile);
+app.get("/abeliophyllum.html", serveFile);
+app.get("/images/abeliophyllum.jpg", serveFile);
 
 module.exports = app.handleRequests.bind(app);
